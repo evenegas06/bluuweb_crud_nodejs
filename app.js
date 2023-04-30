db3c27452da172bb62c3acc61d5196a5000194df
Eduardo Venegas <evenegas06@hotmail.com>
2023-04-30 00:04:36 -0600

    create a new pet #12

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 
 const web_router = require('./router/web');
 const pets_router = require('./router/pets');
 
+const app = express();
 const port = 3000;
+
+/* --- Middleware --- */
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
 
 /* --- DB Connection --- */
 mongoose.connect('mongodb://127.0.0.1:27017/vet')
--- router/pets.js
+++ router/pets.js
@@ -3,18 +3,37 @@ const router = express.Router();
 
 const Pet = require('../models/pet');
 
+// list all.
 router.get('/', async (request, response) => {
     try {
         const pets_array = await Pet.find();
-        console.log(pets_array);
 
         response.render('pets', {
             pets_array
         });
-
     } catch (error) {
         console.log(error);
     }
+});
+
+// Show the form for creating a new resource.
+router.get('/crear', (request, response) => {
+    response.render('create');
+});
+
+// Store a new resource in database.
+router.post('/', async (request, response) => {
+    const body = request.body;
+
+    try {
+        const pet = new Pet(body);
+        await pet.save();
+        
+        response.redirect('/mascotas');
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
 module.exports = router;
